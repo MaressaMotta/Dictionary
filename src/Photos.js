1fb2c6d9fbfde0d655d4ee15feb43950475fa3e5
MaressaMotta <91914924+MaressaMotta@users.noreply.github.com>
2022-04-29 22:40:33 +0100

    img problem solved

--- src/Photos.js
+++ src/Photos.js
@@ -4,20 +4,24 @@ import "./Photos.css";
 export default function Photos(props) {
   if (props.photos) {
     console.log(props.photos);
-    return(
-    <section className="Photos">
-      <div className="row">
-        {props.photos.map(function (photo, index) {
-          return (
-            <div className="col-4 key={index}">
-              <a href={photo.src.original} target="_blank" rel="noreferrer">
-                <img src={photo.src.landscape} className="img-fluid" />
-              </a>
-            </div>
-          );
-        })}
-      </div>
-    </section>
+    return (
+      <section className="Photos">
+        <div className="row">
+          {props.photos.map(function (photo, index) {
+            return (
+              <div className="col-4 key={index}">
+                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                  <img
+                    src={photo.src.landscape}
+                    alt="description"
+                    className="img-fluid"
+                  />
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </section>
     );
   } else {
     return null;
