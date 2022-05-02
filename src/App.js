959fbb7e71fdc95c7ca42a66ba114430577f1846
MaressaMotta <91914924+MaressaMotta@users.noreply.github.com>
2022-05-02 09:18:24 +0100

    footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.png";
+import logo from "./heartbook.png";
 import "./App.css";
 import Dictionary from "./Dictionary";
 
@@ -6,13 +6,30 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="heartbook" />
       </header>
       <main>
-        <Dictionary defaultKeyword="love" />
+        <Dictionary defaultKeyword="Hello" />
       </main>
       <footer className="App-footer">
-        <small>Coded by Maressa Motta</small>
+        This project was coded by{" "}
+        <a
+        title="Instagram"
+          href="https://www.instagram.com/maressa.codes/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Maressa Motta
+        </a>{" "}
+        and is{" "}
+        <a
+        title="Github"
+          href="https://github.com/MaressaMotta/weather-react"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>{" "}
       </footer>
     </div>
   );
