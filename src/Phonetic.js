4afb84da490c605043a2f3b2ce44a6b6f81bd6eb
MaressaMotta <91914924+MaressaMotta@users.noreply.github.com>
2022-05-04 19:02:29 +0100

    logo

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,9 +2,12 @@ import React from "react";
 import "./Phonetic.css";
 
 export default function Phonetic(props){
+    console.log(props.phonetic.audio);
 return (
     <div className="Phonetic">
-        <a href="{props.phonetic.audio}" target="_blank" rel="noreferrer">
+        <a href="{props.phonetic[0].audio}" 
+        target="_blank" 
+        rel="noreferrer">
             Listen
             </a>
             {props.phonetic.text}
