1b12014a98241b4565ef6be91188356a9ac01af0
MaressaMotta <91914924+MaressaMotta@users.noreply.github.com>
2022-05-06 18:41:59 +0100

    phonetic audio fixed

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,17 +1,14 @@
 import React from "react";
 import "./Phonetic.css";
 
-export default function Phonetic(props){
-    console.log(props.phonetic.audio);
-return (
+export default function Phonetic(props) {
+  console.log(props.phonetic.audio);
+  return (
     <div className="Phonetic">
-        <a href="{props.phonetic[0].audio}" 
-        target="_blank" 
-        rel="noreferrer">
-            Listen
-            </a>
-            {props.phonetic.text}
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+        Listen
+      </a>
+      {props.phonetic.text}
     </div>
-  
-);
+  );
 }
